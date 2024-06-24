d446ac5bba7da4bdb98aa8474980acdd68c872c1
Cole Sashkin <csashceh1024@gmail.com>
2024-06-23 17:15:20 -0700

    update

--- file_analysis/elf_disasm/headers/elf64_header.py
+++ file_analysis/elf_disasm/headers/elf64_header.py
@@ -20,3 +20,18 @@ class ELF64_Hdr(ctypes.Structure):
         ("e_shnum", ctypes.c_uint16),
         ("e_shstrndx", ctypes.c_uint16)
     ]
+
+
+class ELF64:
+
+    def __init__(self, filename):
+        self.data = self._get_data(filename)
+        self.elf_hdr = self._unpack_elf_hdr()
+        
+    def _unpack_elf_hdr(self):
+        pass
+
+    def _get_data(self, filename):
+        with open(filename, "rb") as fh:
+            data = fh.read()
+        return data
