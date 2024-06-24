084a348cd1978d4aa7733c0b85ebebcd2aa29654
cole sashkin <csashceh1024@gmail.com>
2024-06-23 20:07:31 -0700

    update

--- file_analysis/uf2_disasm/headers/uf2_header.py
+++ file_analysis/uf2_disasm/headers/uf2_header.py
@@ -1,14 +1,13 @@
 import ctypes
-import capstone as cs
-import lief
+import struct
 
 
 MAGIC_START0 = 0x0A324655
 MAGIC_START1 = 0x9E5D5157
 MAGIC_END = 0x0AB16F30
 
-#uf2 header
-class UF2_BLOCK(ctypes.Structure):
+#uf2 512 byte block
+class UF2_Hdr(ctypes.Structure):
     
     _fields_ = [
         # 32 byte header
@@ -20,6 +19,10 @@ class UF2_BLOCK(ctypes.Structure):
         ("blockNo", ctypes.c_uint32),
         ("numBlocks", ctypes.c_uint32),
         ("fileSize", ctypes.c_uint32), # or familyID
+    ]
+
+class UF2_Data(ctypes.Structure):
+    _fields_ = [
         # if MCU page size is more than 476 bytes, bootloader should support any payload size
         # if MCU page size is less than 476 bytes, the payload should be a multiple of page size
         ("data", ctypes.c_uint8*476), # data 476 bytes padded with zeros
@@ -30,6 +33,13 @@ class UF2:
 
     def __init__(self, filename):
         self.data = self._get_data(filename)
+        self.uf2_hdr0 = self._unpack_uf2_hdr(self.data[0:32])
+
+    def _unpack_uf2_hdr(self, data):
+        uf2_struct = struct.Struct("8L")
+        uf2_data = uf2_struct.unpack(data)
+        uf2_block = UF2_Hdr(*uf2_data)
+        return uf2_block
 
     def _get_data(self, filename):
         with open(filename, "rb") as fh:
