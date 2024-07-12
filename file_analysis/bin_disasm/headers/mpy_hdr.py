0ff5ef16cc4152496be4b0ad11cde85fd280670b
Cole Sashkin <csashceh1024@gmail.com>
2024-07-12 13:27:59 -0700

    update

--- file_analysis/bin_disasm/headers/mpy_hdr.py
+++ file_analysis/bin_disasm/headers/mpy_hdr.py
@@ -11,3 +11,20 @@ class MPY_HDR(ct.Structure):
         ("feature_flags", ct.c_uint8),
         ("int_size", ct.c_uint8)
     ]
+
+class MPY:
+
+    def __init__(self, filename):
+        self.data = self._get_data(filename)
+        self.mpy_hdr = self.unpack_mpy_hdr()
+
+    def unpack_mpy_hdr(self):
+        hdr_struct = struct.Struct("4B")
+        hdr_data = hdr_struct.unpack(self.data[0:4])
+        hdr = MPY_HDR(*hdr_data)
+        return hdr
+
+    def _get_data(self, filename):
+        with open(filename, "rb") as fh:
+            data = fh.read()
+        return data
