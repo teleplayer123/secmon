3af6b0875de2995c1c1ec9b3dffcffef9e525f7f
cole sashkin <csashceh1024@gmail.com>
2024-07-14 16:23:34 -0700

    update

--- interrogate/platform_utils/win32_utils.py
+++ interrogate/platform_utils/win32_utils.py
@@ -8,8 +8,8 @@ class Win32Util:
         self.cache = {}
 
     def run_cmd(self, cmd):
-        cmd = "powershell.exe {}".format(cmd) 
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding="utf-8")
+        cmd = "powershell.exe -c {}".format(cmd) 
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
         res, err = p.communicate()
         return res or err
     
@@ -17,6 +17,14 @@ class Win32Util:
         for cmd in cmds:
             res = self.run_cmd(cmd)
             print(res)
+
+    def get_firewall_rule_names(self):
+        rule_names = []
+        names = self.run_cmd("Get-NetFirewallRule | select -Property DisplayName")
+        for name in names.split("\n"):
+            if name not in rule_names:
+                rule_names.append(name.strip())
+        return rule_names[3:] 
 
     def list_services(self):
         cmd = "reg query \"HKLM\\SYSTEM\\CurrentControlSet\\services\""
@@ -37,4 +45,13 @@ class Win32Util:
         cap_file = file_info.split(" ")[2]
         res = self.run_cmd(f"pktmon etl2txt {cap_file}")
         return res
+    
+    def get_env_vars(self):
+        evars = []
+        cmd = "Get-ChildItem -Path env:* | select -Property Name"
+        res = self.run_cmd(cmd)
+        for evar in res.split("\n"):
+            if evar not in evars:
+                evars.append(evar.strip())
+        return evars[3:]
     
