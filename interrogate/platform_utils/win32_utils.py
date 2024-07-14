ff3e41b1f999937a884895cf675159c2088fa77c
cole sashkin <csashceh1024@gmail.com>
2024-07-14 15:05:42 -0700

    update

--- interrogate/platform_utils/win32_utils.py
+++ interrogate/platform_utils/win32_utils.py
@@ -8,26 +8,37 @@ class Win32Util:
         self.cache = {}
 
     def run_cmd(self, cmd):
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding="latin-1")
+        cmd = "powershell.exe {}".format(cmd) 
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding="utf-8")
         res, err = p.communicate()
         return res or err
     
+    def run_cmds(self, cmds):
+        for cmd in cmds:
+            res = self.run_cmd(cmd)
+            print(res)
+
     def list_services(self):
-        cmd = "powershell.exe reg query \"HKLM\\SYSTEM\\CurrentControlSet\\services\""
+        cmd = "reg query \"HKLM\\SYSTEM\\CurrentControlSet\\services\""
         return self.run_cmd(cmd)
     
     def get_wifi_log(self):
-        cmd = "powershell.exe Get-WinEvent -Path C:\\Windows\\System32\\LogFiles\\WMI\\Wifi.etl -Oldest > test_wifi.txt"
+        cmd = "Get-WinEvent -Path C:\\Windows\\System32\\LogFiles\\WMI\\Wifi.etl -Oldest > test_wifi.txt"
         res = self.run_cmd(cmd)
         return res
 
     def win_pkt_cap(self, ip, proto="TCP", cap_time=10):
         """requires admin privilages"""
-        subprocess.check_output(f"pktmon filter add -i {ip} -t {proto}", shell=True, encoding="utf-8")
-        subprocess.check_output("pktmon start --etw", shell=True, encoding="utf-8")
+        # subprocess.check_output(f"pktmon filter add -i {ip} -t {proto}", shell=True, encoding="utf-8")
+        # subprocess.check_output("pktmon start --etw", shell=True, encoding="utf-8")
+        cmds = [f"pktmon filter add -i {ip} -t {proto}", "pktmon start --etw"]
+        self.run_cmds(cmds)
         sleep(cap_time)
-        file_info = subprocess.check_output("pktmon stop", shell=True, encoding="utf-8")
+        file_info = self.run_cmd("pktmon stop")
+        # file_info = subprocess.check_output("pktmon stop", shell=True, encoding="utf-8")
         file_info = file_info.split("\n")[-2]
         cap_file = file_info.split(" ")[2]
-        res = subprocess.check_output(f"pktmon etl2txt {cap_file}", shell=True, encoding="utf-8")
+        # res = subprocess.check_output(f"pktmon etl2txt {cap_file}", shell=True, encoding="utf-8")
+        res = self.run_cmd(f"pktmon etl2txt {cap_file}")
         return res
+    
