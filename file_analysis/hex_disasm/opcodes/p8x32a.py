b6f64082e31198d03a8d39db470949d7e39f86bf
Cole Sashkin <csashceh1024@gmail.com>
2024-07-15 10:32:02 -0700

    update

---
+++ file_analysis/hex_disasm/opcodes/p8x32a.py
@@ -0,0 +1,13 @@
+import ctypes as ct
+import struct
+
+
+"""
+0 1:    Zeros (0) and ones (1) mean binary 0 and 1.
+i:      Lower case “i” denotes a bit that is affected by immediate status.
+d s:    Lower case “d” and “s” indicate destination and source bits.
+?:      Question marks denote bits that are dynamically set by the compiler.
+---:    Hyphens indicate items that are not applicable or not important.
+..:     Double-periods represent a range of contiguous values.
+"""
+
