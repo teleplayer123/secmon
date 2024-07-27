e0d1c12b6f3ac73e1b7b02fb0b50e0b7986f6f72
cole sashkin <csashceh1024@gmail.com>
2024-07-27 12:38:36 -0700

    update

--- device_usage/process_usage.py
+++ device_usage/process_usage.py
@@ -9,15 +9,18 @@ class ProcessUsage:
     def iter_win_svcs(self):
         svcs = {}
         for p in psutil.win_service_iter():
-            d = p.as_dict()
-            svcs[p.name()] = {
-                "display_name": d["display_name"],
-                "binpath": d["binpath"],
-                "username": d["username"],
-                "start_type": d["start_type"],
-                "status": d["status"],
-                "pid": d["pid"],
-                "name": d["name"],
-                "description": d["description"]
-            }
+            try:
+                d = p.as_dict()
+                svcs[p.name()] = {
+                    "display_name": d["display_name"],
+                    "binpath": d["binpath"],
+                    "username": d["username"],
+                    "start_type": d["start_type"],
+                    "status": d["status"],
+                    "pid": d["pid"],
+                    "name": d["name"],
+                    "description": d["description"]
+                }
+            except FileNotFoundError:
+                continue
         return svcs
