b58d09e4b852aad611e43b411ada1f6ece073fc8
Cole Sashkin <csashceh1024@gmail.com>
2025-06-17 11:24:38 -0700

    update

--- file_analysis/extract_pcap_data.py
+++ file_analysis/extract_pcap_data.py
@@ -1,4 +1,6 @@
 from scapy.all import *
+from scapy.layers.dot11 import Dot11
+from collections import OrderedDict
 
 def get_pcap_pkts(pcap_file):
     """
@@ -35,6 +37,22 @@ def get_pcap_info(pcap_file):
     
     return packet_data
 
+def sort_pkts_seqnum(pkts):
+    """
+    Use ordered dict to sort packets by sequence number.
+    :param pkts: List of packets.
+    :return: Ordered dict of packets.
+    """
+    sorted_pkts = OrderedDict()
+    for pkt in pkts:
+        try:
+            dot11_layer = pkt.getlayer(Dot11)
+            seq = dot11_layer.SC >> 4
+            sorted_pkts[seq] = pkt
+        except IndexError:
+            continue
+    return sorted_pkts
+
 def extract_pcap_data(pcap_file):
     """
     Extracts packet data from a pcap file and returns it as a list of dictionaries.
@@ -46,11 +64,15 @@ def extract_pcap_data(pcap_file):
     if len(packet_data) == 0:
         raise ValueError("No packets found in the pcap file.")
     result = []
-    for pkt in packet_data:
-        data = pkt[Raw].load
+    for seq, pkt in packet_data.items():
+        try:
+            data = pkt[Raw].load
+        except IndexError:
+            data = None
         if data:
             data = data.decode(errors='ignore')
             result.append(data)
         else:
             continue
-    return "\n".join(result)
+    result = "".join(result)
+    return result
